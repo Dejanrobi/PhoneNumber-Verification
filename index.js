0d76a7f68539c6cde6bea898c932fdeeb874b703
Dejanrobi <dejanrobi93@gmail.com>
2022-09-26 18:59:27 +0300

    Second Changes
    Second changes made on phone number verification

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //FIREBASE CONFIGURATION
 
 //importing firebase services
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js"
-import {getAuth, onAuthStateChanged, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js"
+import { getAuth, onAuthStateChanged, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 
 const firebaseApp = initializeApp({
   //configuration details
@@ -32,22 +32,22 @@ const auth = getAuth(firebaseApp)
 
 //INITIALIZING THE PLUGIN TO GIVE US THE COUNTRY CODES
 const phoneInputField = document.querySelector("#phone");
-  const phoneInput = window.intlTelInput(phoneInputField, {
-    utilsScript:
-      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
 
 //A function to add the form submit
 const info = document.querySelector(".alert-info");
 const submitForm = document.querySelector("#login");
 const verifyDiv = document.querySelector(".verify-div");
-const verifyForm  = document.querySelector(".verify-form");
+const verifyForm = document.querySelector(".verify-form");
 
 
 
 //INVISIBLE RECAPTURE
 
-const generateRecaptcha=()=>{
+const generateRecaptcha = () => {
   window.recaptchaVerifier = new RecaptchaVerifier('recapture-container', {
     'size': 'invisible',
     'callback': (response) => {
@@ -60,7 +60,7 @@ const generateRecaptcha=()=>{
 
 
 
-submitForm.addEventListener("submit", (e)=>{
+submitForm.addEventListener("submit", (e) => {
   e.preventDefault()
   //console.log("Form submitted")
 
@@ -77,31 +77,36 @@ submitForm.addEventListener("submit", (e)=>{
 
   let appVerifier = window.recaptchaVerifier;
   //After the recaptcha is generated, we will then sign in the user using the phone number that we have generated above
-  
+
   //SIGN IN WITH PHONE NUMBER
-  signInWithPhoneNumber(auth, phoneNumber, appVerifier).then((confirmationResult)=>{
+  signInWithPhoneNumber(auth, phoneNumber, appVerifier).then((confirmationResult) => {
     window.confirmationResult = confirmationResult
 
-  }).catch((e)=>{
+  }).catch((e) => {
     const eMessage = e.message
     alert(eMessage)
   })
-  
+
 })
 
 
 
-verifyForm.addEventListener("submit", (e)=>{
+verifyForm.addEventListener("submit", (e) => {
   e.preventDefault()
-  const verificationCode = document.querySelector(".verification-code").value 
+  const verificationCode = document.querySelector(".verification-code").value
   // console.log(verificationCode)
-  confirmationResult.confirm(verificationCode).then((result)=>{
-    alert("User signed in Successfully")
-    const user = result.user
-  }).catch((e)=>{
-    const eMessage = e.message
-    alert(eMessage)
-  })
+
+  //VERIFY OTP
+  //you can make a variable global through e.g. window.confirmationResult
+  let confirmationResult = window.confirmationResult;
+  confirmationResult.confirm(verificationCode).then((result) => {
+    // User signed in successfully.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // User couldn't sign in (bad verification code?)
+    // ...
+  });
 })
 
 // //Including the Recaptcha widget
